55dedea3b64ac12c598c5d68f7c0997e943b55db
Alecrack-123 <janneralejandroa.rojasr@uqvirtual.edu.co>
2025-03-19 13:42:44 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.futbollll {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.futbollll to javafx.fxml;
+    exports co.edu.uniquindio.poo.futbollll;
+}
